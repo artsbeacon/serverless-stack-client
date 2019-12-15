da7ce5dde0d0d1bba39b50075bdf652969ea7a7a
Jeremy Dost <jdost@sharpdotinc.com>
2019-12-15 13:43:26 -0500

    add login and register

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import NotFound from "./containers/NotFound";
 
-export default function Routes() {
-  return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      { /* Finally, catch all unmatched routes */ }
-      <Route component={NotFound} />      
-    </Switch>
-  );
+export default function Routes({ appProps }) {
+    return (
+      <Switch>
+        <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+        <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+        <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+        { /* Finally, catch all unmatched routes */ }
+        <Route component={NotFound} />
+      </Switch>
+    );
 }
