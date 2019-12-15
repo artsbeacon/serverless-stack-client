f0991324e7e4b847cda5aeeb01c9c0313e747b87
Jeremy Dost <jdost@sharpdotinc.com>
2019-12-15 08:37:19 -0500

    setup icons, bootstrap, react router, amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+      BUCKET: "jed-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://xbz1yzxia8.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_IRfONk7fZ",
+      APP_CLIENT_ID: "3a5igk8fo9f9gi7d3ga46up44m",
+      IDENTITY_POOL_ID: "us-east-1:259b969b-e536-4241-96b6-0ae09abc1e8c"
+    }
+};
